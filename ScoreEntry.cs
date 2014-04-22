bf600854d01e3117b20d41f7c458c1d76987eda2
nbebic <nikola.bebic99@gmail.com>
2014-04-22 15:14:24 +0200

    Added ScoreEntry class
    Signed-off-by: nbebic <nikola.bebic99@gmail.com>

---
+++ ScoreEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MasterMindrw2
+{
+    [DataContract]
+    public struct ScoreEntry
+    {
+        [DataMember]
+        public string name { get; private set; }
+        [DataMember]
+        public int time { get; private set; }
+        [DataMember]
+        public int attempts { get; protected set; }
+
+        public ScoreEntry(string name, int time, int atm)
+        {
+            this.name = name;
+            this.time = time;
+            this.attempts = atm;
+        }
+    }
+}
